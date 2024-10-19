38979f343634d828af388aafe1abad4ecc22f1c0
anoop <anoopaathreya@gmail.com>
2024-10-19 18:43:17 +0530

    fix

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-# Install Chrome dependencies for Puppeteer
-sudo apt-get update
-sudo apt-get install -y wget gnupg --no-install-recommends
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo apt-get update
-sudo apt-get install -y google-chrome-stable --no-install-recommends
+
+# Install necessary dependencies for Puppeteer (Chrome)
+apt-get update
+apt-get install -y wget curl gnupg ca-certificates
+
+# Add the official Google Chrome repository key and source
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
+# Install Chrome
+apt-get update
+apt-get install -y google-chrome-stable
